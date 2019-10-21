458c83424d678a7c30d0e7783728f500e03582a4
Tomek Rękawek <rekawek@adobe.com>
2019-10-21 17:16:10 +0200

    Repeat the request to local device.

--- src/data-source/local.cpp
+++ src/data-source/local.cpp
@@ -22,36 +22,40 @@ bool inArray(const String array[], String str) {
 
 void getFromLocalDevice(String url, JsonModel *model) {
   HTTPClient http;
-  Serial.print("[HTTP] begin...\n");
+  Serial.print("[HTTP] Begin...\n");
   http.begin(url);
-  int httpCode = http.GET();
-  Serial.printf("[HTTP] Response code: %d\n", httpCode);
-  if (httpCode > 0) {
+  int httpCode = 0;
+  while (true) {
+    httpCode = http.GET();
     if (httpCode == HTTP_CODE_OK) {
-      String body = http.getString();
+      break;
+    }
+    delay(1000);
+  }
+  Serial.printf("[HTTP] Response code: %d\n", httpCode);
+  
+  String body = http.getString();
 
-      const size_t capacity = JSON_ARRAY_SIZE(15) + 15*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + 490;
-      DynamicJsonDocument doc(capacity);
-      deserializeJson(doc, body);
+  const size_t capacity = JSON_ARRAY_SIZE(15) + 15*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + 490;
+  DynamicJsonDocument doc(capacity);
+  deserializeJson(doc, body);
 
-      JsonArray sensordatavalues = doc["sensordatavalues"];
-      for (auto value : sensordatavalues) {
-        JsonObject o = value.as<JsonObject>();
-        String n = o["value_type"];
-        String v = o["value"];
-        
-        if (inArray(P10_VALUES, n)) {
-          model->pm10 = v.toInt();
-        } else if (inArray(P25_VALUES, n)) {
-          model->pm25 = v.toInt();
-        } else if (inArray(TEMP_VALUES, n)) {
-          model->temp = v.toInt();
-        } else if (inArray(HUMIDITY_VALUES, n)) {
-          model->humidity = v.toInt();
-        } else if (inArray(PRESSURE_VALUES, n)) {
-          model->pressure = v.toInt() / 100;
-        }
-      }
+  JsonArray sensordatavalues = doc["sensordatavalues"];
+  for (auto value : sensordatavalues) {
+    JsonObject o = value.as<JsonObject>();
+    String n = o["value_type"];
+    String v = o["value"];
+    
+    if (inArray(P10_VALUES, n)) {
+      model->pm10 = v.toInt();
+    } else if (inArray(P25_VALUES, n)) {
+      model->pm25 = v.toInt();
+    } else if (inArray(TEMP_VALUES, n)) {
+      model->temp = v.toInt();
+    } else if (inArray(HUMIDITY_VALUES, n)) {
+      model->humidity = v.toInt();
+    } else if (inArray(PRESSURE_VALUES, n)) {
+      model->pressure = v.toInt() / 100;
     }
   }
   http.end();
